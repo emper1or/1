4a7979bdd91fd93130c3edc021d427647aba22e0
emper1or <emper1or38@yandex.ru>
2024-03-07 13:57:37 +0700

    now

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 from tkinter import *
 from tkinter import ttk
-from Buttons import button
-from Labels import label
-import LOGOS
 
 root = Tk()  # создаем корневой объект - окно
 
 root.title("Приложение для решения задания 2 по КСЕ")  # устанавливаем заголовок окна
 root.geometry("600x400+150+90")  # устанавливаем размеры окна
-# root.iconbitmap(default="LOGOS\logo.ico") # не робит потому что пидарас
+root.iconbitmap(default="LOGOS\logo.ico")  # не робит потому что пидарас
 
-b_1 = button.Buttons(text="123", size_x=123, size_y=123, color="red")
-b_1.inspect_button()
 
-l_1 = label.Labels(text="123", size_x=123, size_y=123, color="red")
-l_1.inspect_label()
+def close_window(window):
+    close_button = ttk.Button(window, text="Закрыть окно", command=lambda: window.destroy())
+    close_button.pack(anchor="center", expand=1)
+
+
+def first_click(text, res):
+    window = Tk()
+    window.title(text)
+    window.geometry(res)
+    close_window(window)
+
+
+b_1 = Button(text="First button", command=lambda: first_click(text="New Win", res="600x400"))
+b_1.pack(anchor=CENTER, expand=1)
+
+b_2 = Button()
+
+# l_1 = label.Labels(text="123", size_x=123, size_y=123, color="red")
+# l_1.inspect_label()
 
 root.mainloop()
